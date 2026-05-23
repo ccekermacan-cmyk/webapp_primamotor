83a4886f168d1759533c64e9b7c72523aef62839
William <ccekermacan@gmail.com>
2026-05-23 10:52:04 +0700

    apalah 3

--- src/lib/pocketbase.ts
+++ src/lib/pocketbase.ts
@@ -1,6 +1,9 @@
 import PocketBase from 'pocketbase';
 
-// VITE_PB_URL akan diisi nanti di Vercel. Jika kosong, ia akan pakai localhost.
+// Pastikan VITE_PB_URL di Vercel tidak memiliki trailing slash (/) di akhir
 const pbUrl = import.meta.env.VITE_PB_URL || 'http://127.0.0.1:8090';
 
 export const pb = new PocketBase(pbUrl);
+
+// Tambahkan ini agar tidak terjadi masalah dengan authStore di Vercel
+pb.autoCancellation(false);
